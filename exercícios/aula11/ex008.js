a32ea2aeb163fd8392588cb56357636c33ced866
tuvaluu <132757894+tuvaluu@users.noreply.github.com>
2023-06-11 15:09:48 -0300

    1

--- exercícios/aula11/ex008.js
+++ exercícios/aula11/ex008.js
@@ -1,2 +1,8 @@
-console.log('oioioi') 
-console.log('aryane te amo')
+var vel = 63
+console.log(`A velocidade do seu carro é ${vel}Km/h`) 
+if(vel > 60){
+    console.log(`Voce ultrapassou a velocidade permitida. Multado!`)
+}
+
+
+console.log('')

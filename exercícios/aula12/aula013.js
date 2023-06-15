fd2d5944c1bd5d4a5af74cc246808f322fd50231
tuvaluu <132757894+tuvaluu@users.noreply.github.com>
2023-06-15 14:03:24 -0300

    estudos

---
+++ exercícios/aula12/aula013.js
@@ -0,0 +1,34 @@
+var agora = new Date()
+var diaSem = agora.getDay()
+switch (diaSem) {
+    case 0: 
+    console.log ('Domingo')
+    break
+
+    case 1: ('Segunda-feira')
+    break
+
+    case 2: 
+    console.log ('Terça-feira')
+    break
+
+    case 3: 
+    console.log ('Quarta-feira')
+    break
+
+    case 4: 
+    console.log ('Quinta-feira')
+    break
+
+    case 5: 
+    console.log ('Sexta-feira')
+    break
+
+    case 6: 
+    console.log ('Sabado')
+    break
+
+    default:
+    console.log ('[ERRO] Dia inválido!')
+
+}

7d002222218cf81162008ec0f7b63b7e14e2383f
tuvaluu <132757894+tuvaluu@users.noreply.github.com>
2023-06-21 11:24:20 -0300

    1

---
+++ exercícios/aula14ex/ex016/script.js
@@ -0,0 +1,12 @@
+function contar() {
+    let ini = document.getElementById("txti")
+    let fim = document.getElementById("txtf")
+    let passo = document.getElementById("txtp")
+    let res =  document.getElementById ("res")
+
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        window.alert('[ERRO] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando'
+    }
+}

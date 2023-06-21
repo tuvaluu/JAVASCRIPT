f83d8855b6703301092b502ba5b72e679f8efabe
tuvaluu <132757894+tuvaluu@users.noreply.github.com>
2023-06-21 16:20:19 -0300

    21/06/23

--- exercícios/aula14ex/ex016/script.js
+++ exercícios/aula14ex/ex016/script.js
@@ -4,9 +4,36 @@ function contar() {
     let passo = document.getElementById("txtp")
     let res =  document.getElementById ("res")
 
-    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
-        window.alert('[ERRO] Faltam dados!')
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossivel contar!'
+        
     } else {
+
         res.innerHTML = 'Contando'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value) 
+        if (p <= 0){
+            alert('Paso inválido! Considreando PASSO 1')
+            p = 1
+        }
     }
+        
+        if (i < f) {
+
+            for(let c = i; c <= f; c += p){
+            res.innerHTML +=  ` ${c} \u{1F449} `
+             
+        } else {
+
+            for (let c = i; c >= f; c -= p){
+
+             res.innerHTML += `${c} \u{1F449}`
+
+            }
+        }
+        
+        }
+        
 }
+

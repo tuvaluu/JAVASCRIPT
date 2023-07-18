dd75d836942a67f0dcbf14a9ec57693f07270b8c
tuvaluu <132757894+tuvaluu@users.noreply.github.com>
2023-07-18 10:43:51 -0300

    1

---
+++ adivinhandoonumero/B7WEB/MDL-1/script.js
@@ -0,0 +1,15 @@
+let nota1 = 6
+let nota2 = 6
+let nota3 = 6
+let nota4 = 6
+let media = (nota1 + nota2 + nota3 + nota4) / 4
+console.log(media)
+
+if (media >= 7){
+    console.log("APROVADO!")
+} else {
+    console.log('REPROVADO')
+}
+
+
+
